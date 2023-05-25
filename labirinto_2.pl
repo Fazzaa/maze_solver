95dca07215b6a1cf9f42c8a4d4a07f7544d74928
Fabio Luciani <lu.fa.98@gmail.com>
2023-05-25 19:34:45 +0200

    added more mazes

---
+++ labirinto_2.pl
@@ -0,0 +1,22 @@
+num_col(10).
+num_righe(10).
+
+finale([pos(5,10), pos(10,7)]).
+iniziale(pos(5, 1)).
+
+occupata(pos(3,5)).
+occupata(pos(4,5)).
+occupata(pos(5,5)).
+occupata(pos(6,5)).
+occupata(pos(7,5)).
+occupata(pos(8,5)).
+
+occupata(pos(4,1)).
+occupata(pos(4,2)).
+occupata(pos(4,3)).
+
+occupata(pos(6,1)).
+occupata(pos(6,2)).
+occupata(pos(6,3)).
+occupata(pos(7,3)).
+occupata(pos(8,3)).
