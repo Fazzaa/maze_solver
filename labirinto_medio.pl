0ecb5f3e3ed9e36cb9004cc2e532122a1286574f
Fabio Luciani <lu.fa.98@gmail.com>
2023-05-26 17:03:43 +0200

    modified mazes

--- labirinto_medio.pl
+++ labirinto_medio.pl
@@ -1,10 +1,11 @@
 num_col(20).
 num_righe(20).
 
-finale([pos(6,7), pos(5,18), pos(16,14)]).
+%pos(6,7),
+finale([ pos(5,18), pos(14, 4)]).
 iniziale(pos(1, 1)).
 
-
+occupata(pos(10,1)).
 occupata(pos(10,2)).
 occupata(pos(10,3)).
 occupata(pos(10,4)).
@@ -18,13 +19,23 @@ occupata(pos(10,11)).
 occupata(pos(10,12)).
 occupata(pos(10,13)).
 
-occupata(pos(1,5)).
-occupata(pos(2,5)).
-occupata(pos(3,5)).
-occupata(pos(4,5)).
-occupata(pos(5,5)).
-occupata(pos(6,5)).
-occupata(pos(7,5)).
+occupata(pos(1,2)).
+occupata(pos(2,2)).
+occupata(pos(3,2)).
+occupata(pos(4,2)).
+occupata(pos(5,2)).
+occupata(pos(6,2)).
+occupata(pos(7,2)).
+occupata(pos(8,2)).
+
+occupata(pos(9,4)).
+occupata(pos(8,4)).
+occupata(pos(7,4)).
+occupata(pos(6,4)).
+occupata(pos(5,4)).
+occupata(pos(4,4)).
+occupata(pos(3,4)).
+occupata(pos(2,4)).
 occupata(pos(8,5)).
 occupata(pos(8,6)).
 occupata(pos(8,7)).
