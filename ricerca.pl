b51d05c77c7ba26d94d15b738d051703f688b03c
Fabio Luciani <lu.fa.98@gmail.com>
2023-05-26 17:06:46 +0200

    added bound update

--- ricerca.pl
+++ ricerca.pl
@@ -7,10 +7,11 @@ manhattan(pos(X, Y), pos(XF, YF), Fn, Visitati) :-
 
 initialize :- 
     retractall(current_depth(_)),
+    retractall(next_depth(_)),
     iniziale(S),
-    stato_finale_migliore(S, FinaleMigliore, []), 
-    manhattan(S, FinaleMigliore, Dist, []),
-    assert(current_depth(Dist)).
+    stato_finale_migliore(S, (_, DistanzaSFinaleMigliore), []), 
+    assert(next_depth(DistanzaSFinaleMigliore)),
+    assert(current_depth(DistanzaSFinaleMigliore)).
     
 prova(Cammino) :-
     iniziale(S),
@@ -19,9 +20,7 @@ prova(Cammino) :-
     risolvi(S, Cammino, [], Dist). 
 
 prova(Cammino) :-
-    current_depth(D),
-    DNew is D+1, 
-    DNew < 101,
+    next_depth(DNew),
     retractall(current_depth(_)),
     assert(current_depth(DNew)), 
     prova(Cammino). 
@@ -30,10 +29,13 @@ risolvi(S, [], _, _) :- finale(ListaFinali), member(S,ListaFinali), !.
 
 risolvi(S, [AzioneMigliore|ListaAzioni], Visitati, ProfMax) :-  
     ProfMax > 0,
-    stato_finale_migliore(S, FinaleMigliore, Visitati),
+    current_depth(D),
+    stato_finale_migliore(S, (FinaleMigliore, DSFinaleMigliore), Visitati),
     findall(Az, applicabile(Az, S), AzioniPossibili),
     merge_sort(AzioniPossibili, AzioniPossibiliOrdinate, S, FinaleMigliore, Visitati),
-    member(AzioneMigliore, AzioniPossibiliOrdinate),
+    member((AzioneMigliore, DistanzaStatoMigliore), AzioniPossibiliOrdinate),
+    find_next_limit(D, AzioniPossibiliOrdinate),
+    DistanzaStatoMigliore =< D,
     trasforma(AzioneMigliore, S, SNuovo),
     \+member(SNuovo, Visitati),
     NuovaProfMax is ProfMax-1,  
@@ -41,27 +43,29 @@ risolvi(S, [AzioneMigliore|ListaAzioni], Visitati, ProfMax) :-
 
 
 merge_sort([],[], _, _, _).     % empty list is already sorted
-merge_sort([X],[X], _, _, _).   % single element list is already sorted
+merge_sort([X],[(X, DistanzaX)], pos(XAttuale, YAttuale), pos(XTarget, YTarget), Visitati) :-
+    trasforma(X, pos(XAttuale, YAttuale), pos(XNuovoStatoX, YNuovoStatoX)),
+    manhattan(pos(XNuovoStatoX, YNuovoStatoX), pos(XTarget, YTarget), DistanzaX, Visitati).   % single element list is already sorted
 merge_sort(List,Sorted, pos(XAttuale, YAttuale), pos(XTarget, YTarget), Visitati):-
     List=[_,_|_],divide(List,L1,L2),     % list with at least two elements is divided into two parts
     merge_sort(L1,Sorted1, pos(XAttuale, YAttuale), pos(XTarget, YTarget), Visitati),merge_sort(L2,Sorted2, pos(XAttuale, YAttuale), pos(XTarget, YTarget), Visitati),  % then each part is sorted
     merge(Sorted1,Sorted2,Sorted, pos(XAttuale, YAttuale), pos(XTarget, YTarget), Visitati).                  % and sorted parts are merged
 merge([],L,L, _, _).
 merge(L,[],L, _, _):-L\=[].
-merge([X|T1],[Y|T2],[X|T], pos(XAttuale, YAttuale), pos(XTarget, YTarget), Visitati):-
+merge([(X, DistanzaX)|T1],[(Y, DistanzaY)|T2],[(X, DistanzaX)|T], pos(XAttuale, YAttuale), pos(XTarget, YTarget), Visitati):-
     trasforma(X, pos(XAttuale, YAttuale), pos(XNuovoStatoX, YNuovoStatoX)),
     trasforma(Y, pos(XAttuale, YAttuale), pos(XNuovoStatoY, YNuovoStatoY)),
     manhattan(pos(XNuovoStatoX, YNuovoStatoX), pos(XTarget, YTarget), DistanzaX, Visitati),
     manhattan(pos(XNuovoStatoY, YNuovoStatoY), pos(XTarget, YTarget), DistanzaY, Visitati),
     DistanzaX=<DistanzaY,
-    merge(T1,[Y|T2],T).
-merge([X|T1],[Y|T2],[Y|T], pos(XAttuale, YAttuale), pos(XTarget, YTarget), Visitati):-
+    merge(T1,[(Y, DistanzaY)|T2],T).
+merge([(X, DistanzaX)|T1],[(Y, DistanzaY)|T2],[(Y, DistanzaY)|T], pos(XAttuale, YAttuale), pos(XTarget, YTarget), Visitati):-
     trasforma(X, pos(XAttuale, YAttuale), pos(XNuovoStatoX, YNuovoStatoX)),
     trasforma(Y, pos(XAttuale, YAttuale), pos(XNuovoStatoY, YNuovoStatoY)),
     manhattan(pos(XNuovoStatoX, YNuovoStatoX), pos(XTarget, YTarget), DistanzaX, Visitati),
     manhattan(pos(XNuovoStatoY, YNuovoStatoY), pos(XTarget, YTarget), DistanzaY, Visitati),
     DistanzaX>DistanzaY,
-    merge([X|T1],T2,T).
+    merge([(X, DistanzaX)|T1],T2,T).
     
 divide(L,L1,L2):- 
     length(L, Len),
@@ -72,8 +76,23 @@ split_in_half(L,L1,L2, Half) :-
     length(L1, Half),
     append(L1, L2, L).
 
-stato_finale_migliore(pos(X,Y), StatoFinale, Visitati) :- 
+stato_finale_migliore(pos(X,Y), (StatoFinale, Distanza_1), Visitati) :- 
     finale(ListaStatiFinali),
     member(StatoFinale, ListaStatiFinali),
     manhattan(pos(X,Y), StatoFinale, Distanza_1, Visitati),
     \+ (member(AltroStato, ListaStatiFinali), manhattan(pos(X, Y), AltroStato, Distanza_2, Visitati), Distanza_2 < Distanza_1). 
+
+find_next_limit(SogliaAttuale, [(Az,D) | AzioniPossibiliOrdinate]):-
+    next_depth(NextDepth),
+    member((_, NextFn), [(Az,D) | AzioniPossibiliOrdinate]), 
+    NextFn >= SogliaAttuale,
+    NextFn >= NextDepth,
+    \+ (member((_, AltroFn), [(Az,D) | AzioniPossibiliOrdinate]), AltroFn > SogliaAttuale, AltroFn > NextDepth, AltroFn < NextFn),
+    retractall(next_depth(_)),
+    assert(next_depth(NextFn)).
+
+find_next_limit(SogliaAttuale, [(Az,D) | AzioniPossibiliOrdinate]):-
+    next_depth(NextDepth),
+    member((_, NextFn), [(Az,D) | AzioniPossibiliOrdinate]), 
+    NextFn =< NextDepth,
+    NextFn =< SogliaAttuale.
