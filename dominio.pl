ee2865d463007a4640e729d2ff4bf1a6cfcafcff
Fabio Luciani <lu.fa.98@gmail.com>
2023-05-26 17:03:53 +0200

    modified maze

--- dominio.pl
+++ dominio.pl
@@ -2,7 +2,8 @@ num_col(10).
 num_righe(10).
 
 iniziale(pos(4,2)).
-finale([pos(1,5), pos(4,6)]).
+%finale([pos(1,5), pos(4,6)]).
+finale([pos(7,9), pos(9,2)]).
 occupata(pos(2,5)).
 occupata(pos(3,5)).
 occupata(pos(4,5)).
